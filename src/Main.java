6452158b6d0e0695d82e426d3e137e2e02a41623
pirognepirog <pirognepirog@gmail.com>
2026-03-22 21:39:20 +0300

    Creat Z1 master 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача № 1
-        System.out.println("Задача № 1");
+        System.out.println("Задача № 1    ");
         for (int i = 1; i <= 10; i++){
             System.out.println("Итерация цыкла № " + i + ", число " + i);
         }
